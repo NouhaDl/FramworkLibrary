22082f09e3a39eceecf0b3ae31c30d0185637c14
nouhaila <nouha13adouli@gmail.com>
2025-04-13 15:02:29 +0100

    adjusting package config

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
     name: "FramworkLibrary",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FramworkLibrary",
-            targets: ["FramworkLibrary"]),
+            targets: ["AttijariForum"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "FramworkLibrary"),
-
+        .binaryTarget(
+            name: "AttijariForum",
+            path: "./Sources/discussions/Build/AttijariForum.xcframework"
+        )
     ]
 )
